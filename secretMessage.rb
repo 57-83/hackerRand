ce3e21aed171ac5c7ddceaa9eb1b591ee5318001
+ <+>
2023-02-09 15:04:56 +0300

    completed secretMessage

--- secretMessage.rb
+++ secretMessage.rb
@@ -1,8 +1,3 @@
-# def rot13(secret_messages)
-#     secret_messages.map do |message|
-#       message.tr("A-Za-z", "N-ZA-Mn-za-m")
-#     end
-#   end
 def rot13(secret_messages)
   secret_messages.map do |message|
     message.tr("A-Za-z", "N-ZA-Mn-za-m")
