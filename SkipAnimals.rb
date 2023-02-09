b21931d11193bb6fe748e4984b61d04088286149
+ <+>
2023-02-09 13:20:11 +0300

    completed the third challenge

--- SkipAnimals.rb
+++ SkipAnimals.rb
@@ -5,28 +5,14 @@
 #   end
 #   puts skip_animals(animals, skip)
 
-first_name = "John"
-last_name = "Doe"
-gender = "male"
-occupation = "developer"
-zodiac_sign = "Aries"
-  
-  def validate_user_info(first_name, last_name, gender, occupation, zodiac_sign)
-  if first_name.nil? || first_name.strip.empty?
-    return "First name is required"
-  elsif last_name.nil? || last_name.strip.empty?
-    return "Last name is required"
-  elsif !["male", "female", "other"].include?(gender)
-    return "Invalid gender"
-  elsif occupation.nil? || occupation.strip.empty?
-    return "Occupation is required"
-  elsif zodiac_sign.nil? || zodiac_sign.strip.empty?
-    return "Zodiac sign is required"
-  else
-    return "User information is valid"
+
+def skip_animals(animals, skip)
+    result = []
+    animals.each_with_index do |animal, index|
+      result << "#{index}:#{animal}" unless index < skip
+    end
+    result
   end
-end
+ puts skip_animals(['leopard', 'bear', 'fox', 'wolf'], 2)
+ 
 
-
-
-puts validate_user_info(first_name, last_name, gender, occupation, zodiac_sign)
