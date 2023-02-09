f95361f1cfbf8b31f07a82ea5c5710239e6cd35d
+ <+>
2023-02-10 01:00:17 +0300

    done

---
+++ Arguments.rb
@@ -0,0 +1,7 @@
+def take(array, n=1)
+    # So, in the implementation of the take method, array.drop(n) is used to drop the first n elements from the original array and return the rest of the elements as a new array.
+   array.drop(n)
+  end
+puts take([1, 2, 3, 4, 5],3)
+# So, in the implementation of the take method, array.drop(n) is used to drop the first n elements from the original array and return the rest of the elements as a new array.
+
